5d37d10cff92bfbf732cec2746476d08a3524112
Gregory Vincic <g@7de.se>
2014-02-17 08:14:15 +0100

    Bumping version and release to 0.1.1
    Nothing has changed in the API only addition of docs and tests.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 from setuptools import setup
 
 setup(name='reutil',
-      version='0.1.0',
+      version='0.1.1',
       description="Utilities for working with multiple regular expressions",
       author='Gregory Vincic',
       author_email='g@7de.se',
